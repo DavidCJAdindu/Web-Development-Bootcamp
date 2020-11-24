e21d68c745a66d01156f29c652565e4a7305e998
DavidCJAdindu <lavishdigitalsolutions@gmail.com>
2020-11-24 21:25:41 +0000

    i got stuck, taking a break

--- JAVASCRIPT/Dice Project/index.js
+++ JAVASCRIPT/Dice Project/index.js
@@ -5,3 +5,23 @@ var randomNumber1 = Math.floor((Math.random() * 5) + 1);
 
 // Hint: You will need to use what you've learnt about the DOM and the method setAttribute() to achieve this.
 
+var image1 = document.getElementsByClassName("img1");
+
+if (randomNumber1 == 1) {
+  image1.setAttribute("src", "images/dice1.png");
+}
+else if (randomNumber1 == 2) {
+  image1.setAttribute("src", "images/dice2.png");
+}
+else if (randomNumber1 == 3) {
+  image1.setAttribute("src", "images/dice3.png");
+}
+else if (randomNumber1 == 4) {
+  image1.setAttribute("src", "images/dice4.png");
+}
+else if (randomNumber1 == 5) {
+  image1.setAttribute("src", "images/dice5.png");
+}
+else {
+  image1.setAttribute("src", "images/dice6.png");
+}
