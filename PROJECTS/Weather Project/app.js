73b61739c5fef2e8198e08b86e1f640fa5aeb53c
DavidCJAdindu <lavishdigitalsolutions@gmail.com>
2020-12-09 04:20:42 +0000

    finished weather app

--- PROJECTS/Weather Project/app.js
+++ PROJECTS/Weather Project/app.js
@@ -1,16 +1,28 @@
 const { response } = require("express");
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res){
+app.use(bodyParser.urlencoded({extended: true}));
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=manchester&appid=7257ee045cd98983e9a61db0f4013f72&units=metric"; 
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+
+  const query = req.body.cityName;
+  const apiKey = "7257ee045cd98983e9a61db0f4013f72";
+  const units = "metric"
+  
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+ apiKey +"&units="+ units +""; 
 
   https.get(url, function(response){
 
-    console.log(res.statusCode);
+    console.log(response.statusCode);
 
     response.on("data", function(data){
 
@@ -18,30 +30,69 @@ app.get("/", function(req, res){
 
       const description = weatherData.weather[0].description;
       const temp = weatherData.main.temp;
+      const location = weatherData.name;
+
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon +"@2x.png";
 
 
-      res.write("<h2>The weather is currently</h2>" + description);
-      res.write("<p>The temperature in Manchester is " + temp + " degrees</p>");
+
+      res.write("<h1>The weather is currently " + description + "</h1>");
+      res.write("<p>The temperature in " + location + " is " + temp + " degrees</p>");
+      res.write("<img></img>");
+      res.write("<img src=" + imageURL + ">");
 
       res.send();
 
-      
-
-      
-      
-
+    
     });
 
   });
 
-
-  
-
-
 });
-
 
 
 app.listen(3000, function(){
-  console.log("Running")
+  console.log("Server Running");
 });
+
+
+
+
+/*
+
+const apiKey = "7257ee045cd98983e9a61db0f4013f72";
+  const units = "metric"
+  
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+ apiKey +"&units="+ units +""; 
+
+  https.get(url, function(response){
+
+    console.log(response.statusCode);
+
+    response.on("data", function(data){
+
+      const weatherData = JSON.parse(data);
+
+      const description = weatherData.weather[0].description;
+      const temp = weatherData.main.temp;
+      const location = weatherData.name;
+
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon +"@2x.png";
+
+
+
+      res.write("<h1>The weather is currently " + description + "</h1>");
+      res.write("<p>The temperature in " + location + " is " + temp + " degrees</p>");
+      res.write("<img></img>");
+      res.write("<img src=" + imageURL + ">");
+
+      res.send();
+
+    
+    });
+
+  });
+
+*/
