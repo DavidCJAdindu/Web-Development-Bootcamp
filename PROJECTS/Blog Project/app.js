e64a0dc0cfe0ea768aa167e10ed6ed8305a510ca
DavidCJAdindu <lavishdigitalsolutions@gmail.com>
2020-12-11 00:07:08 +0000

    finished blog project

--- PROJECTS/Blog Project/app.js
+++ PROJECTS/Blog Project/app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -14,9 +15,14 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+const blogPosts = [];
+
 
 app.get("/", function(req, res){
-  res.render("home", {home: homeStartingContent});
+  res.render("home", {
+    home: homeStartingContent,
+    posts: blogPosts,
+  });
 });
 
 app.get("/about", function(req, res){
@@ -31,18 +37,38 @@ app.get("/compose", function(req, res){
   res.render("compose");
 });
 
+app.get("/posts/:page", function(req, res){
+
+  const reqTitle = req.params.page
+  
+  blogPosts.forEach(function(post) {
+
+    const storedTitle = post.title
+
+    if(_.lowerCase(storedTitle) === _.lowerCase(reqTitle)) {
+
+      res.render("post", {
+        newPostTitle: post.title,
+        newPostBody: post.body
+      });
+    }
+
+  });
+
+});
+
 app.post("/compose", function(req, res){
   
   // const newPost = req.body.postTitle;
 
   const blogPost = {
-    blogPostTitle: req.body.postTitle,
-    blogPostBody: req.body.postBody
+    title: req.body.postTitle,
+    body: req.body.postBody
   };
 
-  console.log(blogPost.blogPostTitle);
+  blogPosts.push(blogPost);
 
-  res.redirect("/compose");
+  res.redirect("/");
 
 });
 
